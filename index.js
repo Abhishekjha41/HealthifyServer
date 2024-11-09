f351d87ae1a26fe694630784759eff4a7ecbae8b
Abhishek Jha <abhishekjha6908@gmail.com>
2024-11-09 13:38:33 +0530

    finalize

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import feedbackRoute from "./api/feedback.js";
-import privateSecureRoute from "./api/privatesecure.js";
 import aiInsightsRoute from "./api/ai-powered-insights.js";
 import chatRoute from "./api/chat.js";
+import periodTrackerRoute from "./api/period-tracker.js";
 
 const app = express();
 dotenv.config();
@@ -13,9 +13,9 @@ app.use(express.json());
 
 // Use the routes
 app.use(feedbackRoute);
-app.use(privateSecureRoute);
 app.use(aiInsightsRoute);
 app.use(chatRoute);
+app.use(periodTrackerRoute);
 
 const PORT = 5000;
 app.listen(PORT, () => {
